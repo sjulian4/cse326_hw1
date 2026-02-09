ccd36374d1cad4b74dd2b8c4f56b77910146e2bc
sjulian4 <sarah.julian@stonybrook.edu>
2026-02-08 19:29:19 -0500

    progress

--- hw1.py
+++ hw1.py
@@ -2,6 +2,7 @@
 # equation: f(x) = e^{-x^4}-x^3+cos(1-x^2)
 # data: https://docs.google.com/spreadsheets/d/1ISUL8f31Oi0aGR5STT-P3AiG6PJPRuMBblmrMWydlx0/edit?usp=sharing
 import numpy as np
+import time
 
 def f(x):
     return np.exp(-x**4) - x**3 - np.cos(1 - x**2)
@@ -13,13 +14,17 @@ x_true = 0.54075
 
 def main():
 
+
+    bisection_start_time = time.perf_counter()
     # run bisection method with [-1,1]
     x_low = -1.0
     x_high = 1.0
     iters0 = int(1.0e4)
+    bisection_flops = 0
 
     for i in range(iters0):
         x_mid = (x_low + x_high) / 2.0
+        
         if abs(f(x_mid)) < 5.0e-5:
             print("Bisection method: approximate root is " + str(x_mid) + ". Iterations: " + str(i+1) + ".\nRelative error: " + str(100.0 * abs(x_mid - x_true) / x_true) + "%.")
             break
@@ -28,7 +33,11 @@ def main():
         elif f(x_low) * f(x_mid) < 0.0:
             x_high = x_mid
 
+    bisection_end_time = time.perf_counter()
+    print("Bisection method took " + str(bisection_end_time - bisection_start_time) + " seconds.")
 
+
+    newton_start_time = time.perf_counter()
     # run Newton's method with initial guess x0 =0.1111
 
     iters1 = int(1.0e3)
@@ -38,7 +47,11 @@ def main():
         if abs(f(x0)) < 5.0e-5:
             print("Newton's method: approximate root is " + str(x0) + ". Iterations: " + str(i+1) + ".\nRelative error: " + str(100.0 * abs(x0 - x_true) / x_true) + "%.")
             break
+    newton_end_time = time.perf_counter()
+    print("Newton's method took " + str(newton_end_time - newton_start_time) + " seconds.")
 
+
+    secant_start_time = time.perf_counter()
     # run secant method with x_0 = -1 and x_1 = 1
     iters2 = int(1.0e3)
     x_0 = -1.0
@@ -50,8 +63,12 @@ def main():
         if abs(f(x_1)) < 5.0e-5:
             print("Secant method: approximate root is " + str(x_1) + ". Iterations: " + str(i+1) + ".\nRelative error: " + str(100.0 * abs(x_1 - x_true) / x_true) + "%.")
             break
+    secant_end_time = time.perf_counter()
 
-    
+    print("Secant method took " + str(secant_end_time - secant_start_time) + " seconds.")    
+
+
+    monte_carlo_start_time = time.perf_counter()
     # run monte carlo method with [0.3,0.7]
     np.random.seed(2)
 
@@ -61,6 +78,10 @@ def main():
             print("Monte Carlo method: approximate root is " + str(guess_arr[i]) + "Iterations: " + str(i+1) + ".\nRelative error: " + str(100.0 * abs(guess_arr[i] - x_true) / x_true) + "%.")
             break
 
+    monte_carlo_end_time = time.perf_counter()
+
+
+    print("Monte Carlo method took " + str(monte_carlo_end_time - monte_carlo_start_time) + " seconds.")
 
 
 if __name__ == "__main__":
