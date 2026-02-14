fe94cd68f4a4a6ffcfddbe8ab83f3c800effe15f
sjulian4 <sarah.julian@stonybrook.edu>
2026-02-14 17:18:27 -0500

    progress part 2 and 3

--- hw1.2.py
+++ hw1.2.py
@@ -1,4 +1,6 @@
 import numpy as np
+import matplotlib.pyplot as plt # need to install matplotlib
+
 
 # time t = 1 --> Jan 26
 # t = 2 --> Jan 27. etc.
@@ -20,41 +22,52 @@ import numpy as np
 t_values = np.array([1,2,3,4,5])
 prices = np.array([5015,5190,5400,5396,4865]) # the 'y' value, and rightmost matrix on the page 6 of topic 2 notes
 
-A = np.column_stack([np.ones(len(t_values)), t_values, t_values**2, t_values**3, t_values**4])
-# the above is the matrix with the different powers of x's from page 6 of the topic 2 noes (the leftmost matrix)
-
-coefficients = np.linalg.solve(A, prices)
 
 def P4(t):
-    return coefficients[0] + coefficients[1]*t + coefficients[2]*(t**2) + coefficients[3]*(t**3) + coefficients[4]*(t**4) 
+    func = 0.0
+    for i in range(prices.size):
+        coefficient = 1
+        for j in range(prices.size):
+            if j != i:
+                coefficient *= (t - t_values[j]) / (t_values[i] - t_values[j])
+        func += prices[i] * coefficient
+    return func
 
 
-print("P_4(t) = " + str(P4))
+x_arr = np.linspace(1.0, 6.0, 100)
+plt.scatter(t_values, prices, color='blue', label='Data')
+plt.plot(x_arr, P4(x_arr), color='red', label=r'$P_4(t)$')
+plt.xlabel('t (days)')
+plt.ylabel('Gold spot price ($)')
+plt.legend()
+plt.savefig('./P4interpolate.png', dpi=600)
 
 print("P_4(6) = " + str(P4(6)))
 
 
 
-
-
 # (2) Make a quadratic fit of the data Q_2(t) = a_0 + a_1t + a_2t^2 and compute Q_2(t = 6) using Q_2(t).
 
-# A^TAc = A^Ty
-# y = c_1 + c_2t + c_3t^2
+X = np.zeros((prices.size, 3))
+# establish matrix of data
+for i in range(X.shape[0]):
+    for j in range(X.shape[1]):
+        X[i,j] += t_values[i]**j
 
-A2 = np.column_stack([np.ones(len(t_values)), t_values, t_values**2])
 
-ATA = A2.T @ A2
-ATy = A2.T @ prices
+## implement OLS
+a = np.matmul(np.matmul(np.linalg.inv(np.matmul( X.transpose(), X )), X.transpose()), prices)
+def Q_2(x):
+    return (a[0] + a[1]*x + a[2]*x**2)
 
-qcoefficients = np.linalg.solve(ATA, ATy)
+plt.figure()
+plt.scatter(t_values, prices, color='blue', label='Data')
+plt.plot(x_arr, Q_2(x_arr), color='red', label=r'$Q_2(t)$')
+plt.xlabel('t (days)')
+plt.ylabel('Gold spot price ($)')
+plt.legend()
+plt.savefig('./Q2fit.png', dpi=600)
 
-def Q2(t):
-    return qcoefficients[0] + qcoefficients[1] * t + qcoefficients[2] * (t**2)
- 
-print("Q_2(t) = " + str(Q2)) # todo fix the prints of the functions
-
-print("Q_2(6) = " + str(Q2(6)))
 
 
 
--- hw1a.py
+++ hw1a.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 # Jan 16, 2026 -> $410.50
 # Jan 20, 2026 -> $415.80
@@ -17,22 +18,37 @@ import numpy as np
 
 # cubic goes up to 3
 
-t = np.arange(1,11)
 
-# prices (y)
-prices = np.array([410.50, 415.80, 422.30, 428.15, 432.60, 435.20, 430.90, 431.46, 416.56, 430.41])
+t_arr = np.array([16.0, 20.0, 21.0, 22.0, 23.0, 26.0, 27.0, 28.0, 29.0, 30.0]) - 15 # this is b/c some data points have multiple days between
+## set the first day as t=1
+y_arr = np.array([410.5, 415.8, 422.3, 428.15, 432.6, 435.2, 430.9, 431.46, 416.56, 430.41])
 
-A = np.column_stack([np.ones(len(t)), t, t**2, t**3])
+X = np.zeros((y_arr.size, 4))
 
-ATA = A.T @ A
-ATy = A.T @ prices
+#establish the matrix
+for i in range(X.shape[0]):
+    for j in range(X.shape[1]):
+        X[i,j] += t_arr[i] ** j
 
-cofficients = np.linalg.solve(ATA, ATy)
+# implement OLS
 
-def C(t):
-    return cofficients[0] + cofficients[1] * t + cofficients[2] * (t**2) + cofficients[3] * (t**3)
+a = np.matmul(np.matmul(np.linalg.inv(np.matmul(X.transpose(), X)), X.transpose()), y_arr)
 
-print("Cubic fit for Tesla stock: " + str(cofficients[0]) + " + " + str(cofficients[1]) + " * t" + " + " + str(cofficients[2]) + " * (t^2) " + str(cofficients[3]) + " * (t^3)")
+def Q3(x):
+    return (a[0] + a[1]*x + a[2]*x**2 + a[3]*x**3)
+
+x_arr = np.linspace(1.0, 16.0, 100)
+
+plt.ylim(390, 460) # to match providfed figure
+plt.scatter(t_arr, y_arr, color='blue', label='Data')
+plt.plot(x_arr, Q3(x_arr), color='red', label=r'$Q_3(t)$')
+plt.xlabel('t (days)')
+plt.ylabel('Gold spot price ($)')
+plt.legend()
+plt.savefig('./Q3fit.png', dpi=600)
 
 
-# todo are we supposed to plot?
+
+
+
+
